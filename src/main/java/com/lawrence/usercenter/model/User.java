e2a5b005297d7643e6c3f8f5f27abaa2d8509f40
bloodymanicure <lawrence02230711@gmail.com>
2023-10-20 00:51:19 +0800

    init

---
+++ src/main/java/com/lawrence/usercenter/model/User.java
@@ -0,0 +1,11 @@
+package com.lawrence.usercenter.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
